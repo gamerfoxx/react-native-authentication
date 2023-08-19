04318f6fa5355494faa60ccecedafce5f6d7f654
gamerfoxx <admin@josephludbrook.com>
2023-08-19 15:54:54 -0400

    add protected data

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,26 +1,40 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 function WelcomeScreen() {
-  return (
-    <View style={styles.rootContainer}>
-      <Text style={styles.title}>Welcome!</Text>
-      <Text>You authenticated successfully!</Text>
-    </View>
-  );
+	const [fetchedMessage, setFetchedMessage] = useState('');
+	useEffect(() => {
+		axios
+			.get(
+				'https://react-native-auth-371b4-default-rtdb.firebaseio.com/message.json'
+			)
+			.then((response) => {
+				setFetchedMessage(response.data);
+			});
+	}, []);
+	return (
+		<View style={styles.rootContainer}>
+			<Text style={styles.title}>Welcome!</Text>
+			<Text>You authenticated successfully!</Text>
+			<Text>{fetchedMessage}</Text>
+		</View>
+	);
 }
 
 export default WelcomeScreen;
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 32,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
+	rootContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 32,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginBottom: 8,
+	},
 });
